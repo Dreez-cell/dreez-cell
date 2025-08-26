16143a23a4a837d1809b4eb3846dcf5951cb96fe
Dreez-cell <crissnyagaka@gmail.com>
2025-08-26 10:46:38 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
-const CACHE_NAME = 'testagram-cache-v1';
+const CACHE_NAME = 'testagram-cache-v2';
+
+// Files & routes to cache
 const urlsToCache = [
-  '/',
-  '/index.html'
+  '/',                           // homepage
+  '/favicon.ico',                // favicon
+  '/manifest.json',              // manifest
+  '/IMG_20250820_144445_(512_x_512_pixel).jpg', // app icon
+  'https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap'
 ];
 
 // Install service worker
@@ -11,9 +16,32 @@ self.addEventListener('install', event => {
   );
 });
 
-// Fetch from cache
+// Activate (cleanup old caches)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch: Cache-first with network fallback
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).then(fetchRes => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
   );
 });
